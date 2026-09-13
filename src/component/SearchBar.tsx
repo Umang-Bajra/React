ab4bac2d32d0e7c06865800b2ede79fd8c8c8ea4
Umang-Bajra <umangbarjacharya41@gmail.com>
2026-09-13 16:57:36 +0545

    adjusting search bar

--- src/component/SearchBar.tsx
+++ src/component/SearchBar.tsx
@@ -1,12 +1,12 @@
 function SearchBar(){
     return(
         <>
-        <div className='flex justify-center m-20 border border-gray-500 rounded-xl '>
-            <div>+</div>
-            <div className='border border-gray-500 h-6 w-100 rounded-xl m-1'> <input type="text" placeholder="ask ChatGPT"/></div>
-            <div className='flex gap-4 justify-end'>
-            <div><img src="mic.png" alt="logo" className='h-8 w-8'></img></div>
-            <div><img src="uparrow.png" alt="logo" className='h-8 w-8'></img></div>
+        <div className='relative flex justify-center items-center mx-auto h-8  w-[70%] border border-gray-500 rounded-xl gap-2 '>
+            <div className=' absolute left-1'>+</div>
+            <div className='absolute left-3 border border-gray-500 h-6 w-[80%] rounded-xl m-1'> <input type="text" placeholder="ask ChatGPT"/></div>
+            <div className='absolute right-1 flex gap-2 justify-end'>
+                <div><img src="mic.png" alt="logo" className=' h-8 w-8'></img></div>
+                <div><img src="uparrow.png" alt="logo" className=' h-8 w-8'></img></div>
             </div>
         </div>
         </>
